4880988814e67991e0428d51efd256b1e8768775
SiresCloud <110801363+SiresCloud@users.noreply.github.com>
2022-08-24 07:49:54 +0300

    Update Linux_QemuWindows110.sh

--- Linux_QemuWindows110.sh
+++ Linux_QemuWindows110.sh
@@ -3,7 +3,7 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
-echo Идёт процесс взлома Genesis Cloud...
+echo Please wait for installing...
 sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
@@ -12,7 +12,7 @@ curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCO
 echo "Windows 10 x86 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "ВНИМАНИЯ: Не нажимайте Ctrl+C для копирования!!!! Перепишите IP вручную!"
-echo "Окно с Putty не закрывать. Когда в окне Putty появится красная надпись (неактивно) - можно закрывать."
-echo Скрипт сделан Артёмом Андреевым
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+echo Script by fb.com/thuong.hai.581
 sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=4  -m 11192M -machine usb=on -device usb-tablet > /dev/null 2>&1
