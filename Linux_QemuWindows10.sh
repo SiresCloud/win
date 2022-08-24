745cc78c110c359b0d156e530b4d32373de16a4d
SiresCloud <110801363+SiresCloud@users.noreply.github.com>
2022-08-24 09:22:20 +0300

    Update Linux_QemuWindows10.sh

--- Linux_QemuWindows10.sh
+++ Linux_QemuWindows10.sh
@@ -15,7 +15,7 @@ echo "Установка QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Установка Windows Disk...
 curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.3/providers/qemu.box
-echo "Windows 7 x86 Установлен!"
+echo "Windows 10 x86 Установлен!"
 echo Ваш VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "ВНИМАНИЯ: Не нажимайте Ctrl+C для копирования!!!! Перепишите IP вручную!"
