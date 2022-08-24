be9050466d3cf82036f45a075237d5c73cec9f46
SiresCloud <110801363+SiresCloud@users.noreply.github.com>
2022-08-24 07:42:20 +0300

    Update Linux_QemuWindows10.sh

--- Linux_QemuWindows10.sh
+++ Linux_QemuWindows10.sh
@@ -1,4 +1,3 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 sudo apt install unzip
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
